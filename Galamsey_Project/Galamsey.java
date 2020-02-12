05102bb7b7a3b82b753111cfbb0252c1b1b1e28c
Stephen Torku <stephentorku@Stephens-MacBook-Pro.local>
2020-02-12 12:34:48 +0000

    Still working on it

--- Galamsey_Project/Galamsey.java
+++ Galamsey_Project/Galamsey.java
@@ -1,4 +1,50 @@
 package Galamsey_Project;
 
+import java.util.Date;
 public class Galamsey {
+    public static enum Vegetation_color{GREEN, YELLOW, BROWN};
+
+    private Vegetation_color vegetation_color;
+    private int veg_col_value;
+    private double[] postion;
+    private Date year;
+
+    
+
+    public Galamsey(int veg_col_value, double[] position, Vegetation_color vegetation_color, Date year){
+        this.postion = position;
+        this.year = year;
+        this.veg_col_value = veg_col_value;
+        this.vegetation_color = vegetation_color;
+    }
+
+    public int getVeg_col_value() {
+        return veg_col_value;
+    }
+
+    public void setVeg_col_value(int veg_col_value) {
+        this.veg_col_value = veg_col_value;
+    }
+
+    public Date getYear() {
+        return year;
+    }
+
+    public void setYear(Date year) {
+        this.year = year;
+    }
+
+    public String getPosition(){
+        
+    }
+
+
+    // @Override
+    // public String toString(){
+    //     String details = "The vegetation colour at this location is:" + this.vegetation_color
+    //     + ", which gives a colour value of: " + getVeg_col_value() +
+    //      "This "
+        
+
+    // }
 }
--- Galamsey_Project/Observatory.java
+++ Galamsey_Project/Observatory.java
@@ -1,4 +1,62 @@
 package Galamsey_Project;
 
+import java.util.Date;
+import java.util.List;
+
 public class Observatory {
+
+    private String observatory_name;
+    private String country_name;
+    private Date year_commenced;
+    private int area_covered_km;
+    private List events;
+
+    public Observatory(String observatory_name, String country_name, Date year_commenced, int area_covered_km, List events){
+        super();
+        this.observatory_name = observatory_name;
+        this.country_name = country_name;
+        this.year_commenced = year_commenced;
+        this.area_covered_km = area_covered_km;
+        this.events = events;
+    }
+
+    public String getObservatory_name() {
+        return observatory_name;
+    }
+
+    public void setObservatory_name(String observatory_name) {
+        this.observatory_name = observatory_name;
+    }
+
+    public String getCountry_name() {
+        return country_name;
+    }
+
+    public void setCountry_name(String country_name) {
+        this.country_name = country_name;
+    }
+
+    public Date getYear_commenced() {
+        return year_commenced;
+    }
+
+    public void setYear_commenced(Date year_commenced) {
+        this.year_commenced = year_commenced;
+    }
+
+    public int getArea_covered_km() {
+        return area_covered_km;
+    }
+
+    public void setArea_covered_km(int area_covered_km) {
+        this.area_covered_km = area_covered_km;
+    }
+
+    public List getEvents() {
+        return events;
+    }
+
+    public void setEvents(List events) {
+        this.events = events;
+    }
 }
