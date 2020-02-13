5d84c07f1be60ed7f270a8f500c397ddac37662f
Dredayduncan <dredayduncan@gmail.com>
2020-02-13 16:12:47 +0000

    Comitting..

--- Galamsey_Project/Monitoring.java
+++ Galamsey_Project/Monitoring.java
@@ -1,4 +1,19 @@
 package Galamsey_Project;
 
+import CSC313_project_EarthquakeMonitoring_10742022.Galamsey_Project.DBConnect;
+
+import java.util.ArrayList;
+
 public class Monitoring {
+    private DBConnect connect;
+    private ArrayList<String> observatories = new ArrayList<>();
+
+    public Monitoring(){
+        connect = new DBConnect();
+    }
+
+    public String largeGalColVal(){
+
+    }
+
 }
