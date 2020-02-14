4e850bcedff6d7175247bda248933f7ec254bad6
Nana Osei <osei.somuah2015@gmail.com>
2020-02-14 21:55:12 +0000

    errors

---
+++ Galamsey_Project/Galamsey_data.sql
@@ -0,0 +1,26 @@
+create database Galamsey_data;
+
+use Galamsey_data;
+
+create table Observatory(
+	obs_name varchar(255) not null,
+    country varchar(255),
+    area_in_sqkm decimal,
+    startYear smallint not null,
+    averageColValue decimal,
+    primary key (obs_name)
+);
+
+create table Galamsey(
+	veg_color enum("Green", "'Yellow", "Brown"),
+    col_value tinyint not null,
+    longitude decimal,
+    latitude decimal,
+    year smallint not null,
+    obs_name varchar(255),
+    foreign key (obs_name) references Observatory(obs_name)
+    
+);
+
+
+    
